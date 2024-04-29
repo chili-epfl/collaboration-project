00751026266981575907e9c1db349b0a14913e39
Liototo <74362833+Liototo@users.noreply.github.com>
2024-04-29 08:12:57 +0200

    Little corrections

--- packages/collaboration/src/cellTracker.ts
+++ packages/collaboration/src/cellTracker.ts
@@ -12,6 +12,16 @@ export function trackActivity(nb: NotebookPanel) {
 
     notebook.activeCellChanged.connect(onCellChanged);
 
+    // When closing the window, remove activity from last cell's metadata
+    window.addEventListener('beforeunload', () => {
+        if (prevCell && !disconnected) {
+    
+            removeActivity(prevCell);
+            disconnected = true;
+    
+        }
+    })
+
 }
 
 export function stopTracking(nb: NotebookPanel | null) {
@@ -34,25 +44,7 @@ function onCellChanged() {
         if (prevCell) removeActivity(prevCell);
 
         // When entering a new cell, add activity to its metadata
-        if (cell) {
-
-            addActivity(cell);
-
-            let activeUsers = cell.model.getMetadata('active_users');
-
-            console.log('This cell has', activeUsers.toString(), 'active users');
-
-        }
-
-        // When closing the window, remove activity from last cell's metadata
-        window.addEventListener('beforeunload', () => {
-            if (prevCell && !disconnected) {
-
-                removeActivity(prevCell);
-                disconnected = true;
-
-            }
-        })
+        if (cell) addActivity(cell);
 
         prevCell = cell;
 
@@ -77,6 +69,7 @@ function removeActivity(cell: Cell) {
 
     let activeUsers = cell.model.getMetadata('active_users');
     activeUsers--;
+    if (activeUsers < 0) activeUsers = 0;
     cell.model.setMetadata('active_users', activeUsers);
 
 }
